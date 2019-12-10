8c2935947318008263aa6873dc00beb3b9dbd7bd
Maciek Pekala <m@peka.la>
2019-12-10 22:46:01 +0100

    Add lingui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { Trans, Plural } from "@lingui/macro";
 
-const App = () => <div>Hello World!</div>;
+let a = "World";
+let messagesCount = 10;
+const App = () => (
+  <div>
+    <Trans>
+      Hello {a} {Math.random()}!
+    </Trans>
+    <p>
+      <Plural
+        value={messagesCount}
+        one="There's # message in your inbox"
+        other="There're # messages in your inbox"
+      />
+    </p>
+  </div>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { I18nProvider } from "@lingui/react";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <I18nProvider language="en">
+    <App />
+  </I18nProvider>,
+  document.getElementById("app")
+);
