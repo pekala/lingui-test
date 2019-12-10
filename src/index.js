a513945ec5590ddd48cb9dd45c0e36c4343293f4
Maciek Pekala <m@peka.la>
2019-12-10 23:48:57 +0100

    Add loading of catalogs on demand

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { I18nProvider } from "@lingui/react";
-import polish from "./locales/pl/messages.po";
-import english from "./locales/en/messages.po";
 
 import App from "./App";
 
 const AppWithLang = () => {
   const [lang, setLang] = React.useState("en");
+  const [catalogs, setCatalogs] = React.useState({});
+
+  React.useEffect(() => {
+    function loadCatalog() {
+      import(
+        /* webpackMode: "lazy", webpackChunkName: "i18n-[index]" */
+        `./locales/${lang}/messages.po`
+      ).then(newCatalog => setCatalogs({ ...catalogs, [lang]: newCatalog }));
+    }
+    loadCatalog();
+  }, [lang]);
+
+  const usedLang = !!catalogs[lang] ? lang : catalogs["en"] ? "en" : null;
+
+  if (!usedLang) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <I18nProvider language={lang} catalogs={{ pl: polish, en: english }}>
+    <I18nProvider language={usedLang} catalogs={catalogs}>
       <button onClick={() => setLang("pl")}>PL</button>
       <button onClick={() => setLang("en")}>EN</button>
       <App />
