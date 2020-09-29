a1f97d0f5dee1912ba5dc9fba2ba83e706db65a0
Igor Dolzhenkov <igor.techweb@gmail.com>
2020-09-29 11:21:59 +0300

    Hello -translate-

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App = () => {
         <Trans>Make nosds push</Trans>
         <Trans>NEW LdsdsdsdsgfsdffdfsffsfdsfdfdfsdsdsafsdfsfOCALE3232</Trans>
         <Trans>Saved123123 world</Trans>
-        <Trans>Hi guy</Trans>
+        <Trans>Hifwef guy</Trans>
         <Trans>Loli</Trans>
         <Trans>NEW LOCALE</Trans>
         <Trans>NEW LOsdfsdfsdf CALE</Trans>
