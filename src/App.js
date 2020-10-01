9389d131ccad31a72daf3b480aece4d63ccceae0
Igor Dolzhenkov <igor.techweb@gmail.com>
2020-10-01 17:54:26 +0300

    Save

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
         <Trans>Saved123123 world</Trans>
         <Trans>Hifweffwfefef guy</Trans>
         <Trans>Lolicdcpwfwefwefe</Trans>
-        <Trans>NEW LOCALE</Trans>
+        <Trans>NEW LOCALEsd fsdf</Trans>
         <Trans>NEW LOsdfsdfsdf CALE</Trans>
         <Trans>Okey Gffdoogle</Trans>
         <Trans>Go gore go</Trans>
